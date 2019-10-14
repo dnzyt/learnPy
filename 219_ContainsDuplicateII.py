f31cd26cd6423da5853bb5df6613a95fc68bd0f6
dnzyt <dnzyt1986@gmail.com>
2019-10-14 11:02:49 -0400

    new solutions

---
+++ 219_ContainsDuplicateII.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        lookup = {}
+        for i, num in enumerate(nums):
+            if num not in lookup:
+                lookup[num] = i
+            else:
+                if i - lookup[num] <= k:
+                    return True
+                lookup[num] = i
+        return False
