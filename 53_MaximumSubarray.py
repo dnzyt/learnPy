be3605856df403c20718906c42eac5f800756695
dnzyt <dnzyt1986@gmail.com>
2020-01-05 11:43:54 -0500

    New Solutions

--- 53_MaximumSubarray.py
+++ 53_MaximumSubarray.py
@@ -8,3 +8,9 @@ class Solution:
             if localMax > 0:
                 globalMax = max(localMax, globalMax)
         return globalMax
+
+    def maxSubArray2(self, nums):
+        dp = nums
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i], nums[i] + dp[i - 1])
+        return max(dp)
