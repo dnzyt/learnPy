e28bcb5a21a284d8db3f845a039da4999a47937e
dnzyt <dnzyt1986@gmail.com>
2020-03-28 22:29:37 -0400

    New Solutions

---
+++ 59_SpiralMatrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def generateMatrix(self, n):
+        matrix = []
+        for i in range(n):
+            matrix.append([0 for j in range(n)])
+        rowBegin = 0
+        rowEnd = n - 1
+        colBegin = 0
+        colEnd = n - 1
+        number = 1
+
+        while rowBegin <= rowEnd and colBegin <= colEnd:
+            for i in range(colBegin, colEnd + 1):
+                matrix[rowBegin][i] = number
+                number += 1
+            rowBegin += 1
+
+            for i in range(rowBegin, rowEnd + 1):
+                matrix[i][colEnd] = number
+                number += 1
+            colEnd -= 1
+
+            for i in range(colEnd, colBegin - 1, -1):
+                matrix[rowEnd][i] = number
+                number += 1
+            rowEnd -= 1
+
+            for i in range(rowEnd, rowBegin - 1, -1):
+                matrix[i][colBegin] = number
+                number += 1
+            colBegin += 1
+        return matrix
+                
+
